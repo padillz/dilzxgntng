620ff709d448d0f34aba4207da1c547e5a9504f5
dilzx <padillz575@gmail.com>
2025-12-12 19:49:08 +0700

    Update token list

--- tokendark.js
+++ tokendark.js
@@ -2,6 +2,7 @@
   "tokens": [
     "122344555:hgxghjkkkknnjj",
     "8498069200:AAE7CT4OM5GNSx0oi8RyUKS-09tzrJmlBXM",
-    "8001616412:AAE40A1EYv7OXIuVuP7MXgqrvvT0V0-VkTg"
+    "8001616412:AAE40A1EYv7OXIuVuP7MXgqrvvT0V0-VkTg",
+    "7587697728:AAFbtQnY56apFd2IZ7u3YfutEBcyjexmWHg"
   ]
 }
